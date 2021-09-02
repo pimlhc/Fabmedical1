a6c276a0fb097bac17ac7d4fa3f1ce0cfacbf8ab
pimlhc <36978038+pimlhc@users.noreply.github.com>
2021-09-02 06:42:39 +0000

    Update app.js

--- content-web/app.js
+++ content-web/app.js
@@ -6,7 +6,7 @@ const request = require('request');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
-const contentApiUrl = process.env.CONTENT_API_URL || "http://52.226.18.227:3001";
+const contentApiUrl = process.env.CONTENT_API_URL || "http://40.83.246.252:3001";
 
 
 function getSessions(cb) {
